9303be4c1346b715108c6161061e2732e56ed0a0
gimsieke <gimsieke@d851d441-0421-4803-b127-45cb279a3ef2>
2013-03-11 22:29:22 +0000

    restricting query to some path

--- crossref/trunk/sample.sh
+++ crossref/trunk/sample.sh
@@ -1,1 +1,1 @@
-calabash/calabash.sh -i source=out.xml -o qb=qb.xml email=X@Y user=USER pass=PASS crossref/xpl/jats-submit-crossref-query.xpl
+calabash/calabash.sh -i source=bits.xml -o qb=query_batch.xml email=X@Y user=USER pass=PASS crossref/xpl/jats-submit-crossref-query.xpl xpath='(//sec)[last()]'
